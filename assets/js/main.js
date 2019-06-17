3a3de34f167b4c0966924905e58a2d50fe50cf34
Ariannys Garcia <ariannysgarcia18@gmail.com>
2019-06-17 14:57:11 -0400

    add others view

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,6 +86,16 @@ $(document).ready(function () {
   });
 });
 
+wow = new WOW(
+  {
+    boxClass: 'wow',      // default
+    animateClass: 'animated', // default
+    offset: 0,          // default
+    mobile: false,       // default
+    live: true        // default
+  }
+)
+wow.init();
 
 
 
